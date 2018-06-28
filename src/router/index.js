b4be1b3dcec72a35256596dc09c5a9a1d7972049
KC Wesley <KCWx9@tuta.io>
2018-06-28 13:31:46 +0100

    add Home.vue

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,6 @@ import Router from 'vue-router'
 
 import Home from '@/views/Home'
 
-import Articles from '@/views/Articles'
-import Index from '@/views/Index'
-import Library from '@/views/Library'
-import Framework from '@/views/Framework'
-
 Vue.use(Router)
 
 export default new Router({
@@ -17,26 +12,6 @@ export default new Router({
       name: 'home',
       path: '/',
       component: Home
-    },
-    {
-      name: 'articles',
-      path: '/articles',
-      component: Articles
-    },
-    {
-      name: 'index',
-      path: '/index',
-      component: Index
-    },
-    {
-      name: 'library',
-      path: '/library',
-      component: Library
-    },
-    {
-      name: 'framework',
-      path: '/framework',
-      component: Framework
     }
   ]
 })
