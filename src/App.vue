d9122d872c53d6cae411ee0dabaf8099626231f7
KC Wesley <KCWx9@tuta.io>
2018-06-28 13:09:32 +0100

    tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <nav-bar class="z-50"></nav-bar>
+    <router-view class="z-20"/>
   </div>
 </template>
 
 <script>
+import '@/assets/css/master.css'
+
+import NavBar from '@/components/NavBar'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    NavBar
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,42 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import Home from '@/views/Home'
+
+import Articles from '@/views/Articles'
+import Index from '@/views/Index'
+import Library from '@/views/Library'
+import Framework from '@/views/Framework'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
+      name: 'home',
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: Home
+    },
+    {
+      name: 'articles',
+      path: '/articles',
+      component: Articles
+    },
+    {
+      name: 'index',
+      path: '/index',
+      component: Index
+    },
+    {
+      name: 'library',
+      path: '/library',
+      component: Library
+    },
+    {
+      name: 'framework',
+      path: '/framework',
+      component: Framework
     }
   ]
 })
