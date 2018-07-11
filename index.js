542b2c03f7213d898e1d68a4c0a131951ee3f993
KC Wesley <KCWx9@tuta.io>
2018-07-11 10:55:31 +0100

    *

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // ---------------------------------------------------- Base Config
-require('dotenv').config();
 
-// const config = require('./config/config');
-const db = require('./config/database');
+const config = require('./config/config');
 const app = require('./config/express');
+const db = require('./config/database');
 
 module.exports = app;
