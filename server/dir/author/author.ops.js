b90a9851780f24cede46e803b8c46a7a96c1e924
KC Wesley <KCWx9@tuta.io>
2018-07-20 15:10:03 +0100

    changed book.ops

--- server/dir/author/author.ops.js
+++ server/dir/author/author.ops.js
@@ -8,46 +8,51 @@ exports.createAuthor = (req, res) => {
   author.date_of_death = req.body.date_of_death;
 
   author.save((err) => {
-    if (err)
-      res.status(400).send(err);
-
-    res.status(200).json({ success: true, message: 'Author created.' });
+    if (err) {
+      return res.status(400).send(err);
+    } else {
+      return res.status(200).json({ success: true, message: 'Author created.' });
+    }
   });
 };
 
 exports.findAuthors = (req, res) => {
   Author.find((err, authors) => {
-    if (err)
-      res.status(400).send(err);
-
-    res.status(200).json(authors);
+    if (err) {
+      return res.status(400).send(err);
+    } else {
+      return res.status(200).json(authors);
+    }
   });
 };
 
 exports.findAuthorById = (req, res) => {
   Author.findById(req.params._id, (err, author) => {
-    if (err)
-      res.status(400).send(err);
-
-    res.status(200).json(author);
+    if (err) {
+      return res.status(400).send(err);
+    } else {
+      return res.status(200).json(author);
+    }
   });
 };
 
 exports.updateAuthor = (req, res) => {
   Author.findById(req.params._id, (err, author) => {
-    if (err)
-      res.status(400).send(err);
-
-    author.first_name = req.body.first_name;
-    author.last_name = req.body.last_name;
-    author.date_of_birth = req.body.date_of_birth;
-    author.date_of_death = req.body.date_of_death;
+    if (err) {
+      return res.status(400).send(err);
+    } else {
+      author.first_name = req.body.first_name;
+      author.last_name = req.body.last_name;
+      author.date_of_birth = req.body.date_of_birth;
+      author.date_of_death = req.body.date_of_death;
+    }
 
     author.save((err) => {
-      if (err)
-        res.status(400).send(err);
-
-      res.status(200).json({ success: true, message: 'Author updated.'});
+      if (err) {
+        return res.status(400).send(err);
+      } else {
+        return res.status(200).json({ success: true, message: 'Author updated.'});
+      }
     });
   });
 };
@@ -56,9 +61,10 @@ exports.deleteAuthor = (req, res) => {
   Author.remove({
     _id: req.params._id
   }, (err, author) => {
-    if (err)
-      res.status(400).send(err);
-
-    res.status(200).json({ success: true, message: 'Author deleted.'})
-  })
+    if (err) {
+      return res.status(400).send(err);
+    } else {
+      return res.status(200).json({ success: true, message: 'Author deleted.'})
+    }
+  });
 };
